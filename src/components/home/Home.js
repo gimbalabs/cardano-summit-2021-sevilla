6d75716f9a1190791ce32bdeb4922008950ed63c
the hub <thehub@Jamess-MacBook-Pro.local>
2021-01-07 08:59:05 -0500

    Home page updates

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -13,12 +13,17 @@ function Home() {
                 </div>
                 <div className="col m-5">
                     <h1>How can you get involved?</h1>
-                    <h4>At GimbaLabs, we want to help you find ways to participate in the adoption of ADA</h4>
+                    <h4></h4>
                     <div class="card m-5">
                         <h5 class="card-header">Help drive adoption of ADA by participating in the Gimbalabs Local ADA Spending Challenge</h5>
                         <div class="card-body">
-                            <h5 class="card-title"></h5>
-                            <p class="card-text">See details here and a video description here.</p>
+                            <h5 class="card-title">At GimbaLabs, we want to help you find ways to participate in the adoption of ADA</h5>
+                            <p class="card-text">
+                                Over the next few months, we will continue to release APIs and Starter Kits that provide entry points for learning about and building upon what Cardano can do. 
+                            </p>
+                            <p class="card-text">
+                                In our most recent Cardano Starter Kit, we provide an iniital look at "token bundles" on Cardano. WHen See details here and a video description here.
+                            </p>
                             <p class="card-text">List of winners</p>
                             <p class="card-text">Why are we doing this?</p>
                             <a href="https://github.com/GimbaLabs/csk-005" class="btn btn-primary m-2">Challenge Description on GitHub</a>
