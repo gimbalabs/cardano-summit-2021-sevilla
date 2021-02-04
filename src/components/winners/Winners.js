579b23a4c68674639e1441866bdaa4f02405edd9
James Dunseith <info@workshopmaybe.com>
2021-02-04 10:05:30 -0500

    Second CSK 005 Winner

--- src/components/winners/Winners.js
+++ src/components/winners/Winners.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 function Winners() {
     return (
-        <div class="container">
+        <div class="container mb-5">
             <div class="col m-5"></div>
             <div class="col m-5">
                 <h1>Winners of the Gimbalabs Local ADA Spending Challenge</h1>
                 <h3>THANK YOU for participating!</h3>
-                <div class="card">
+                <div class="card m-3">
                     <div class="card-header">
                         #1: <a href="https://twitter.com/cole_vt">@cole_vt</a> on January 2, 2021
                     </div>
@@ -15,6 +15,16 @@ function Winners() {
                         <blockquote class="twitter-tweet"><p lang="en" dir="ltr">As part of the <a href="https://twitter.com/MaybeWorkshop?ref_src=twsrc%5Etfw">@MaybeWorkshop</a> local use <a href="https://twitter.com/hashtag/Cardano?src=hash&amp;ref_src=twsrc%5Etfw">#Cardano</a> challenge, I bought lunch today, a Vermonter, Korean fried chicken, and Yerba at Café Saint Paul in Burlington, Vermont with ADA. Their wallet is all set up, and they are now staking with ADOPT! Another successful venture to help</p>&mdash; Cole Bartlett (@cole_vt) <a href="https://twitter.com/cole_vt/status/1345576010205560833?ref_src=twsrc%5Etfw">January 2, 2021</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
                         <blockquote class="twitter-tweet"><p lang="en" dir="ltr">us push the <a href="https://twitter.com/hashtag/adoption?src=hash&amp;ref_src=twsrc%5Etfw">#adoption</a> of <a href="https://twitter.com/hashtag/Cardano?src=hash&amp;ref_src=twsrc%5Etfw">#Cardano</a> <a href="https://twitter.com/hashtag/ADA?src=hash&amp;ref_src=twsrc%5Etfw">#ADA</a> <a href="https://twitter.com/hashtag/ADOPT?src=hash&amp;ref_src=twsrc%5Etfw">#ADOPT</a> <a href="https://twitter.com/AdoptStakePool?ref_src=twsrc%5Etfw">@AdoptStakePool</a> <a href="https://t.co/hGwD1ltlpX">pic.twitter.com/hGwD1ltlpX</a></p>&mdash; Cole Bartlett (@cole_vt) <a href="https://twitter.com/cole_vt/status/1345477893678395398?ref_src=twsrc%5Etfw">January 2, 2021</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 
+                    </div>
+                </div>
+                <div class="card m-3">
+                    <div class="card-header">
+                        #2: <a href="https://twitter.com/jsierrax">@jsierrax</a> on December 31, 2020
+                    </div>
+                    <div class="card-body">
+                        <p>"I gave ADA as Christmas presents for my nephews and nieces," reports @jsierrax. Here at Gimbalabs, we hesitated for just a moment: "does that really count as local spending?" we wondered.</p>
+                        <p>Then we thought about it. What's more "local" than family? We can't think of a better place to start the conversations that will branch out to the world.</p>
+                        <p>Thanks for your thoughtfulness and continued contributions to our community, @jsierrax!</p>
                     </div>
                 </div>
 
